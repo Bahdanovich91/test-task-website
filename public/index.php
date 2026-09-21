8d976ca18f96f5d4a4863b97544115e8a76412cb
Bahdanovich91 <113859381+Bahdanovich91@users.noreply.github.com>
2026-09-21 14:08:33 +0300

    Added autoloading, Fixed Dockerfile

--- public/index.php
+++ public/index.php
@@ -2,16 +2,11 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../app/Core/Database/Database.php';
-require_once __DIR__ . '/../app/Core/Models/Model.php';
-require_once __DIR__ . '/../app/Models/Category.php';
-require_once __DIR__ . '/../app/Models/Post.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Models\Category;
 use App\Models\Post;
 
-$categories = Category::all();
-$posts = Post::all();
+var_dump(Post::all());
+var_dump(Category::all());
 
-var_dump($categories);
-var_dump($posts);
